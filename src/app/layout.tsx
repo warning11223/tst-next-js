1cb61d6353590bc1c823b3762429ff5793159805
warning11223 <warning11223@gmail.com>
2025-10-29 14:27:58 +0300

    fix: build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-import {Providers} from "@/src/components/Providers";
-import {ReactNode} from "react";
-
-const inter = Inter({ subsets: ['latin', 'cyrillic'] });
+import { Providers } from "@/src/components/Providers";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
     title: 'Каталог видео',
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="ru">
-        <body className={inter.className}>
+        <body>
         <Providers>{children}</Providers>
         </body>
         </html>
